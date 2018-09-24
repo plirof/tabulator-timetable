9f50b774c03f5ff5ea7c0b4d9c0ece92cf67ea78
aaa <aaa@aaa.com>
2018-09-24 19:11:27 +0300

    jquery

--- test01.php
+++ test01.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+<script type="text/javascript" src="js/jquery1.12.4.js"></script>
+<script type="text/javascript" src="js/jquery-ui.min.js"></script>	
 <link href="css/tabulator3.3.2.min.css" rel="stylesheet">
 <script type="text/javascript" src="js/tabulator3.3.2.min.js"></script>
 
@@ -7,5 +9,10 @@
 <body>
 
 <div id="example-table"></div>
-<script>var table = new Tabulator("#example-table", {});</script>
+<script>
+$("#example-table").tabulator();
+
+//var table = new Tabulator("#example-table", {});
+
+</script>
 </body>
