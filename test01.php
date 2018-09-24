e6b6c910e60b20fde8da2f8ac4bf636f42dc9877
aaa <aaa@aaa.com>
2018-09-24 19:07:17 +0300

    added tab v4 + some other libs

---
+++ test01.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<link href="css/tabulator3.3.2.min.css" rel="stylesheet">
+<script type="text/javascript" src="js/tabulator3.3.2.min.js"></script>
+
+</head>
+<body>
+
+<div id="example-table"></div>
+<script>var table = new Tabulator("#example-table", {});</script>
+</body>
